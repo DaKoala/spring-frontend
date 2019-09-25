ed85112b80284b1ef557c33251bd53cba639fe77
BillyZou <yz3352@nyu.edu>
2019-09-25 20:16:27 +0800

    feat: 初始化Input组件

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+
+interface InputProps {
+
+}
+
+export default class Input extends PureComponent<InputProps> {
+  render() {
+    return (
+      <div>
+        <label></label>
+      </div>
+    )
+  }
+}
+
