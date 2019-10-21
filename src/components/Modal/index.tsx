a8c4367cb5e6eebb297c4a0a9361e9e31c3a90e6
BillyZou <yz3352@nyu.edu>
2019-10-21 20:11:06 +0800

    feat: 添加Modal组件

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import autobind from 'autobind-decorator'
+import classNames from 'classnames/bind';
+import styles from './index.less';
+
+const cx = classNames.bind(styles);
+
+interface ModalProps {
+  visible: boolean;
+  onMaskClick?: React.MouseEventHandler;
+  children: React.ReactNode;
+}
+
+export default class Modal extends PureComponent<ModalProps> {
+  @autobind
+  handleMaskClick(e: React.MouseEvent) {
+    e.stopPropagation();
+    const { onMaskClick } = this.props;
+    onMaskClick && onMaskClick(e);
+  }
+
+  render() {
+    const { visible, children } = this.props;
+    return visible ? (
+      <div className={cx('modal__mask')} onClickCapture={this.handleMaskClick}>
+        <div className={cx('modal__window')}>
+          {children}
+        </div>
+      </div>
+    ) : null;
+  }
+}
+
